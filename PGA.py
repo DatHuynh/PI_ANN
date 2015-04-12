721765a4a444e3790bd059d7e1b3b30a9c9f06db
Dat Huynh <badat0202@yahoo.com.vn>
2015-04-12 09:32:52 +0700

    New data presentation

--- PGA.py
+++ PGA.py
@@ -4,40 +4,6 @@ from deap import base, creator,tools
 import random
 import numpy as np
 import Network as nw
-import math
-import random
-
-from itertools import repeat
-from collections import Sequence
-
-ranges = [[500,5000],[10,15],[30000,95000],[30,40],[42000,494000],[33,42],[42000,494000],[33,42],[42000,494000],[33,42],[42000,494000],[33,42],[42000,494000],[33,42],[42000,494000],[33,	42],[42000,	494000],[33,	42],[95000,	5600000],[18,	28]]
-
-def initPs(container):
-    min = 0
-    max = 1
-    Ps = container(np.zeros(len(ranges)))
-    for i in range(0,len(ranges)):
-        if i > 0 and i%2 == 0:
-            low = (Ps[i-2]*(ranges[i-2][max]-ranges[i-2][min]) + ranges[i-2][min] - ranges[i][min])/(ranges[i][max]-ranges[i][min])
-            if low > 0:
-                Ps[i] = random.random()*(1.0-low)+low
-            else:
-                Ps[i] = random.random()
-        else:
-            Ps[i] = random.random()
-    return Ps
-
-def adjustConstraint(Ps):
-    min = 0
-    max = 1
-    for i in range(0,len(ranges)):
-        if i > 0 and i%2 == 0:
-            deNormalize1 = Ps[i-2]*(ranges[i-2][max]-ranges[i-2][min]) + ranges[i-2][min]
-            deNormalize2 = Ps[i]*(ranges[i][max]-ranges[i][min])+ranges[i][min]
-            low = (deNormalize1 - ranges[i][min])/(ranges[i][max]-ranges[i][min])
-            if deNormalize1 > deNormalize2:
-                Ps[i] = random.random()*(1.0-low)+low
-
 
 def cxTwoPointCopy(ind1, ind2):
     size = len(ind1)
@@ -59,8 +25,6 @@ def evaluate(ps,net,us):
     for i in range(len(us)):
         r_net += np.power((us_net[i] - us[i])/us[i],2)
     k = np.sqrt(r_net/len(us))
-    if np.isnan(k):
-        print('')
     return k
 
 
@@ -70,11 +34,11 @@ def paraGA(length,net,us,numIndividual,numGeneration,crossOverPB, mutantPB):
     creator.create("Individual",np.ndarray,fitness = creator.FitnessMin)
 
     toolbox = base.Toolbox()
-    toolbox.register("individual",initPs,creator.Individual)
+    toolbox.register("individual",tools.initRepeat,creator.Individual, random.random,length)
     toolbox.register("population",tools.initRepeat,list,toolbox.individual)
 
     toolbox.register("mate", cxTwoPointCopy)
-    toolbox.register("mutate", tools.mutPolynomialBounded, eta = 0.8, low = 0, up = 1, indpb=0.6)
+    toolbox.register("mutate", tools.mutPolynomialBounded, eta=0.5, low = 0, up =1, indpb=0.08)
     toolbox.register("select", tools.selTournament, tournsize=3)
     toolbox.register("evaluate",evaluate,net = net,us = us)
 
@@ -95,15 +59,16 @@ def paraGA(length,net,us,numIndividual,numGeneration,crossOverPB, mutantPB):
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
             if random.random() < crossOverPB:
                 toolbox.mate(child1, child2)
-                adjustConstraint(child1)
-                adjustConstraint(child2)
                 del child1.fitness.values
                 del child2.fitness.values
 
         for mutant in offspring:
+            if not isInRange(mutant,0,1):
+                print('Nan')
             if random.random() < mutantPB:
                 toolbox.mutate(mutant)
-                adjustConstraint(mutant)
+                if not isInRange(mutant,0,1):
+                    print('Nan')
                 del mutant.fitness.values
 
         # Evaluate the individuals with an invalid fitness
--- PlayGround.py
+++ PlayGround.py
@@ -12,8 +12,6 @@ Haven't normalize
 '''
 ps = None
 sizes = [20,10,102]
-#answerPs = [0.2664499,0.5221133,0.88982]
-#10,216000,1.12,30.31
 trainingdata = fh.loadTrainingReal('Ps','Us',sizes[0],sizes[2],9,215999,0.1,31)            #this is the slow down factor
 testdata = None
 usTarget = fh.loadUs('UsTarget',sizes[2],0.1,31)
@@ -22,7 +20,7 @@ t = 1
 
 #while success is False:
 #800
-'''
+print(len(trainingdata))
 print("#PI {}".format(t))
 net = WGA.weightGA(sizes,trainingdata,testdata,eta=0.001,numIndividual= 50,numGeneration=800,numGDMStep=10,crossOverPB= 0.5, mutantPB= 0.2)
 fh.saveWeightJson(net.weights,"WeightsRaw")
@@ -41,19 +39,19 @@ net.GD(trainingdata,None,epoch=100000,eta=0.001)
 fh.saveWeightJson(net.weights,"WeightsOpti")
 fh.saveWeightJson(net.biases,"BiasesOpti")
 
-'''
+
 net = nw.Network(sizes,threshold= 0.02,alpha=0.001)
 net.weights = fh.loadWeightJson("WeightsOpti")
 net.biases = fh.loadWeightJson("BiasesOpti")
-#print("End Of W optimize Pharse - TrainData: {}".format(net.evaluate(trainingdata)))
+print("End Of W optimize Pharse - TrainData: {}".format(net.evaluate(trainingdata)))
 #100
-#ps = PGA.paraGA(sizes[0],net,usTarget, numIndividual=100, numGeneration=500,crossOverPB=0.5,mutantPB=0.3)
-#fh.saveVec('PsSuggest',ps)
-#print("End Of PGA phrase - NetError: {}".format(PGA.evaluate(ps,net,usTarget)))
+ps = PGA.paraGA(sizes[0],net,usTarget, numIndividual=100, numGeneration=500,crossOverPB=0.8,mutantPB=0.8)
+fh.saveVec('PsSuggest',ps)
+print("End Of PGA phrase - NetError: {}".format(PGA.evaluate(ps,net,usTarget)))
 #10000
 ps = fh.loadPs('PsSuggest',sizes[0])
-#ps = PGD.optimizePs(ps,net,usTarget,epoch=10000,eta=0.001)       #eta is not good -- dynamic eta
-#fh.saveVec('PsSuggestOpti',ps)
+ps = PGD.optimizePs(ps,net,usTarget,epoch=10000,eta=0.001)       #eta is not good -- dynamic eta
+fh.saveVec('PsSuggestOpti',ps)
 print("End Of P optimize phrase - NetError: {}".format(PGA.evaluate(ps,net,usTarget)))
 
 confirm = input('Please enter Us Model: ')
